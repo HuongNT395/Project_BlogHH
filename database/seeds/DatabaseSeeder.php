68e3eb2d3c12e2060b34abbc35ddd93814096eee
thuhoai1095 <thuhoai54a.spt@gmail.com>
2017-09-13 11:43:16 +0700

    sua ten class trong seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CategoryTable::class);
-        $this->call(UserTable::class);
-        $this->call(PostTable::class);
+        $this->call(UsersTable::class);
+        $this->call(CategoriesTable::class);
+        $this->call(PostsTable::class);
     }
 }
--- database/seeds/UsersTable.php
+++ database/seeds/UsersTable.php
@@ -12,21 +12,20 @@ class UsersTable extends Seeder
      */
     public function run()
     {
+        $user = new User();
+        $user->name = "Nguyen Thi Huong";
+        $user->email = "huongnt1@nal.vn";
+        $user->password = bcrypt("123456");
+        $user->avatar = "";
+        $user->active = 1;
+        $user->save();
 
         $user1 = new User();
         $user1->name = "To Thi Thu Hoai";
         $user1->email = "hoaittt@nal.vn";
         $user1->password = bcrypt("123456");
-        $user1->avatar = "hoai.jpg";
-        $user1 ->active = 1;
+        $user1->avatar = "";
+        $user1 ->active = 0;
         $user1->save();
-
-        $user = new User();
-        $user->name = "Nguyen Thi Huong";
-        $user->email = "huongnt1@nal.vn";
-        $user->password = bcrypt("123456");
-        $user->avatar = "huong.jpg";
-        $user->active = 0;
-        $user->save();
     }
 }
