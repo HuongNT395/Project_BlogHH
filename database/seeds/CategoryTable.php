44edd117fc8434253ce79b48568b23d9b75f61fb
Huong Nguyen <street.demon95@gmail.com>
2017-09-13 11:19:27 +0700

    add data on CategortTable

--- database/seeds/CategoryTable.php
+++ database/seeds/CategoryTable.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
 class CategoryTable extends Seeder
 {
@@ -11,6 +12,36 @@ class CategoryTable extends Seeder
      */
     public function run()
     {
-        //
+        $category = new Category();
+        $category->name = 'Science';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Star';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Fashion';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Technology';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Sport';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Travel';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Music';
+        $category->save();
+
+        $category = new Category();
+        $category->name = 'Film';
+        $category->save();
     }
 }
