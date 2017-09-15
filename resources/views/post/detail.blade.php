a7a70187b4e5fea863456d133d18ef426b8a6dfc
Huong Nguyen <street.demon95@gmail.com>
2017-09-15 17:03:07 +0700

    xem chi tiet 1 post

---
+++ resources/views/post/detail.blade.php
@@ -0,0 +1,20 @@
+@include('until/header')
+<article id="author" class="row">
+    <div class="au_detail">
+        <div class="col-md-3">
+            <img src="{{asset('storage/images/avartar/'. $post->user->avatar)}}" class="img-circle">
+        </div>
+        <div class="col-md-9">
+            <a href="#"><b>{{$post->user->name}}</b></a><br>
+            Email: <a href="mailto:{{$post->user->email}}">{{$post->user->email}}</a><br>
+            <i>{{$post->updated_at}}</i>
+        </div>
+    </div>
+
+</article>
+<article class="contents row">
+    <img id="images" class="img-responsive" src="{{asset('storage/images/post/'. $post->image)}}">
+    <h1>{{$post->title}}</h1>
+    <h4>{{$post->content}}</h4>
+</article>
+@include('until/footer')
