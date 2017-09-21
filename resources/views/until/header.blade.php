f03d2703c428c1e4b6e989b1470b2f3885d8376c
Huong Nguyen <street.demon95@gmail.com>
2017-09-21 15:04:20 +0700

    done

--- resources/views/until/header.blade.php
+++ resources/views/until/header.blade.php
@@ -8,36 +8,28 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    <link href="{{asset('icon.ico')}}" rel="shortcut icon">
     <title>News</title>
 </head>
 <body>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.10&appId=246006722589120";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
 <div class="container">
     {{--start header--}}
     <header class="row">
         <div class="col-md-3">
-            <a href="{{route('post.logged.addForm')}}">write a story</a>
+            <a href="{{route('login')}}">write a story</a>
         </div>
         <div class="col-md-4">
             <a href="#">News</a>
         </div>
         <div class="col-md-5" style="text-align: center">
-            <form action="{{route('search')}}" method="get" role="search">
-                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <div class="input-group">
-                    <input type="text" name="keyword" placeholder="Search" style="margin-top: 5px">
-                </div>
+            <form>
+                <input type="text" name="search" placeholder="Search">
             </form>
-            <p><a href="">sign in</a> / <a href="#">sign up</a></p>
+            <p><a href="{{route('login')}}">sign in</a> / <a href="#">sign up</a></p>
         </div>
     </header>
+    {{--end header--}}
+    {{--start nav--}}
     <nav class="row">
         <ul>
             <li><a href="">Home</a></li>
@@ -46,5 +38,4 @@
             @endforeach
         </ul>
     </nav>
-</div>
-
+{{--end nav--}}
